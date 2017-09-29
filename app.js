57313da676771801dcc2e605ee31d7e354c2fdac
Leo Huang <hua0005@gmail.com>
2017-09-30 01:18:19 +1000

    fixed async issues with promises, now correctly downloads data file

--- app.js
+++ app.js
@@ -34,59 +34,85 @@ app.post("/ffdi", function(req, res)
 		//Temp files for storing downloaded files
 		var databaseFile = tmp.fileSync({dir: "./"});
 		var dataFile = tmp.fileSync({dir: "./"});
-		let getDataLocation = new Promise(function(resolve, reject)
+		let getDataLocation = function()
 		{
-			client.get(dbLocation, function(err, stream)
+			return new Promise(function(resolve, reject)
 			{
-				//download stations_db.txt
-				if (err)
+				client.get(dbLocation, function(err, stream)
 				{
-					reject(err);
-				}
-				stream.pipe(fs.createWriteStream(__dirname + "/" + databaseFile.name));
-				stream.on("finish", function()
-				{
-					//parse through the stations
-					var fileContents = fs.readFileSync(databaseFile.name, "utf8").toLowerCase();
-					fileContents = fileContents.split("\n");
-					for (var i = 0; i < fileContents.length; i++)
+					//download stations_db.txt
+					if (err)
 					{
-						fileContents[i] = fileContents[i].replace(new RegExp(/([a-z]|\(|\))\ ([a-z]|\(|\))/g), "$1_$2"); //replacing all the (spaces) in (char)(space)(char) with _
-						fileContents[i] = fileContents[i].replace(new RegExp(/\ \/\ /g), "___"); //replace (space)/(space) with ___
-						fileContents[i] = fileContents[i].replace(new RegExp(/_aws/g), ""); //removing all the _aws
-						fileContents[i] = fileContents[i].replace(new RegExp(/(\ )+/g), " "); //replaces all multiple spaces into 1 single space
-						fileContents[i] = fileContents[i].split(" ");
+						reject(err);
 					}
-					//find the neareste weather station
+					stream.pipe(fs.createWriteStream(__dirname + "/" + databaseFile.name));
+					stream.on("finish", function()
+					{
+						//parse through the stations
+						var fileContents = fs.readFileSync(databaseFile.name, "utf8").toLowerCase();
+						fileContents = fileContents.split("\n");
+						for (var i = 0; i < fileContents.length; i++)
+						{
+							fileContents[i] = fileContents[i].replace(new RegExp(/([a-z]|\(|\))\ ([a-z]|\(|\))/g), "$1_$2"); //replacing all the (spaces) in (char)(space)(char) with _
+							fileContents[i] = fileContents[i].replace(new RegExp(/\ \/\ /g), "___"); //replace (space)/(space) with ___
+							fileContents[i] = fileContents[i].replace(new RegExp(/_aws/g), ""); //removing all the _aws
+							fileContents[i] = fileContents[i].replace(new RegExp(/(\ )+/g), " "); //replaces all multiple spaces into 1 single space
+							fileContents[i] = fileContents[i].split(" ");
+						}
+						//find the nearest weather station
+						var closestIndex = 0;
+						var closestDistance = Math.sqrt(Math.pow(fileContents[0][5] - user.longitude, 2) + Math.pow(fileContents[0][6] - user.latitude, 2));
+						for (var index = 1; index < fileContents.length; index++)
+						{
+							var newDistance = Math.sqrt(Math.pow(fileContents[index][5] - user.longitude, 2) + Math.pow(fileContents[index][6] - user.latitude, 2));
+							if (newDistance < closestDistance)
+							{
+								closestDistance = newDistance;
+								closestIndex = index;
+							}
+						}
+						//update the directory name name
+						dataLocation = dataLocation.concat(fileContents[closestIndex][1].concat("/")); //state directory
+						dataLocation = dataLocation.concat(fileContents[closestIndex][3].concat("/")); //station directory
+						dataLocation = dataLocation.concat(fileContents[closestIndex][3].concat("-").concat((new Date()).getFullYear())); //year
+						dataLocation = dataLocation.concat(("0" + ((new Date()).getMonth() + 1)).slice(-2)); //month
+						dataLocation = dataLocation.concat(".csv"); //file extension
+						resolve();
+					});
 				});
 			});
-			resolve();
-		});
-		let getClimateData = new Promise(function(resolve, reject)
+		}
+		let getClimateData = function()
 		{
-			client.get(dataLocation, function(err, stream)
+			return new Promise(function(resolve, reject)
 			{
-				//download climate data file
-				if (err)
+				client.get(dataLocation, function(err, stream)
 				{
-					reject(err);
-				}
-				//stream.pipe(fs.createWriteStream(__dirname + "/" + dataFile.name));
-				stream.on("finish", function()
-				{
-					//parse the data file path and add relevant data to variables
-				});
-				//Cleanup
-				stream.once("close", function()
-				{
-					databaseFile.removeCallback();
-					dataFile.removeCallback();
-					client.end();
+					//download climate data file
+					if (err)
+					{
+						reject(err);
+					}
+					stream.pipe(fs.createWriteStream(__dirname + "/" + dataFile.name));
+					stream.on("finish", function()
+					{
+						//parse the data file path and add relevant data to variables
+					});
+					//Cleanup
+					stream.once("close", function()
+					{
+						databaseFile.removeCallback();
+						dataFile.removeCallback();
+						client.end();
+						resolve();
+					});
 				});
 			});
-			resolve();
+		}
+		getDataLocation().then(function()
+		{
+			return getClimateData();
 		});
-		getDataLocation.then(getClimateData);
 	});
 	client.connect(user);
 });
